33557b35be55cc7961495c52acb0815d1ad5f144
BeomSu-Kang <kbs21c1987@gmail.com>
2022-03-13 19:19:42 +0900

    카운트연습

--- study/src/App.js
+++ study/src/App.js
@@ -1,14 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-import { Fragment } from 'react';
-import MyComponent from './MyComponent';
+import React from 'react';
+import Counter from './Counter.js';
 
 const App = () => {
-  return (
-    <MyComponent name={'React'} favoriteNumber={3}>
-      리액트
-    </MyComponent>
-  );
+  return <Counter />;
 };
 
 export default App;
---
+++ study/src/Counter.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  //state 초기값 설정
+  state = {
+    number: 0,
+    fixedNumber: 0,
+  };
+
+  render() {
+    const { number, fixedNumber } = this.state;
+    return (
+      <div>
+        <h1>{number}</h1>
+        <h2>바뀌지 않는 값 : {fixedNumber}</h2>
+        <button
+          //onclick 호출 함수지정
+          onClick={() => {
+            this.setState((prevState) => {
+              return {
+                number: prevState.number + 1,
+              };
+            });
+            this.setState((prevState) => ({
+              number: prevState.number + 1,
+            }));
+          }}
+        >
+          +1
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
