16192cfc71ebf60de5f845de63c068bfa062afdf
BeomSu-Kang <kbs21c1987@gmail.com>
2022-03-13 19:47:58 +0900

    useState 사용하기, 여러개 가능

--- study/src/App.js
+++ study/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import Counter from './Counter.js';
+// import Counter from './Counter.js';
+import Say from './Say.js';
 
+// const App = () => {
+//   return <Counter />;
+// };
 const App = () => {
-  return <Counter />;
+  return <Say />;
 };
 
 export default App;
