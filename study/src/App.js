2ebaedace424eb404d1487e61778987ace82c68f
BeomSu-Kang <kbs21c1987@gmail.com>
2022-03-13 18:52:29 +0900

    컴포넌트 연습

--- study/src/App.js
+++ study/src/App.js
@@ -1,30 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import { Fragment } from 'react';
+import MyComponent from './MyComponent';
 
-function App() {
-  const name = 'react';
-  const no = undefined;
-  const style = {
-    backgroundColor: '#000',
-    color: 'aqua',
-    fontSize: '20px',
-    fontWeight: 'bold',
-    padding: '16px',
-  };
+const App = () => {
   return (
-    <div>
-      {name === '리액트' ? (
-        <h1>리액트 입니다.</h1>
-      ) : (
-        <h2>리액트가 아닙니다.</h2>
-      )}
-      <h2>안녕!!</h2>
-      <div>{name === '리액트' ? <h1>리액트입니다.</h1> : '아니야'}</div>
-      <div style={style}>{no || '리액트'}</div>
-      <input type="text" />
-    </div>
+    <MyComponent name={'React'} favoriteNumber={3}>
+      리액트
+    </MyComponent>
   );
-}
+};
 
 export default App;
--- study/src/MyComponent.js
+++ study/src/MyComponent.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const MyComponent = () => {
-  return <div>새로운 컴포넌트</div>;
+const MyComponent = ({ name, favoriteNumber, children }) => {
+  return (
+    <div>
+      내이름은 {name}
+      <br />
+      children 값은 {children}
+      <br />
+      내가 좋아하는 숫자는 {favoriteNumber}입니다.
+    </div>
+  );
 };
 
+MyComponent.defaultProps = {
+  name: '강강',
+};
+
+MyComponent.propTypes = {
+  name: PropTypes.string,
+  favoriteNumber: PropTypes.number.isRequired,
+};
+
+//모듈 내보내기
 export default MyComponent;
