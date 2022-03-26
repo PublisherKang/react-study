78a1691d06755e3b9df5eeaf49b850d4311330f0
desktop-kang <kbs21c1987@gmail.com>
2022-03-26 22:14:24 +0900

    input add

--- study/src/App.js
+++ study/src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ValidationSample from './ValidationSample.js';
 // import Counter from './Counter.js';
 // import Say from './Say.js';
-import EventPractice from './EventPractice.js';
+// import EventPractice from './EventPractice.js';
 
 // const App = () => {
 //   return <Counter />;
 // };
-const App = () => {
-  return <EventPractice />;
-};
+// const App = () => {
+//   return <EventPractice />;
+// };
+
+class App extends Component{
+  render(){
+    return (
+      <ValidationSample/>
+    );
+  }
+}
 
 export default App;
